fc43e62a23f2d05d2e67fcbcf1636f05ce4c608a
ghoshArnab <arnabnitb@gmail.com>
2015-01-20 15:34:41 +0530

    jschanges

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function getLocation() {
-   Modernizr.geolocation, $wait.fadeOut(), $locationBar.fadeIn()
+   Modernizr.geolocation, fnWaiting.fadeOut(), placeLoc.fadeIn()
 }
 
 function utilityOption() {
@@ -7,7 +7,7 @@ function utilityOption() {
 }
 
 function currentLocation(a) {
-   $wait.fadeIn(), $locationBar.fadeOut();
+   fnWaiting.fadeIn(), placeLoc.fadeOut();
    var b = a.coords.latitude,
       c = a.coords.longitude;
    currentlatlng = new google.maps.LatLng(b, c), getPlaces(currentlatlng)
@@ -22,36 +22,36 @@ function getPlaces(a) {
    });
    var b = {
       location: a,
-      radius: 5e3,
+      radius: 2e3,
       keyword: utility
    };
    service.search(b, searchUtility)
 }
 
 function searchUtility(a) {
-   barResultsStore = a;
+   resultsArrayCol = a;
    var b = {
       location: currentlatlng,
-      radius: 5e3,
+      radius: 2e3,
       keyword: utility
    };
    service.search(b, searchRequest)
 }
 
 function searchRequest(a) {
-   for (pubResultsStore = a, totalResults = barResultsStore.concat(pubResultsStore), resultsStore = removeDupes(totalResults, "id"), resultsStore = resultsStore.sort(function() {
+   for (resultsArray = a, totalResults = resultsArrayCol.concat(resultsArray), resultsStore = Duplicates(totalResults, "id"), resultsStore = resultsStore.sort(function() {
          return Math.random() - .5
       }), i = 0; i < resultsStore.length; i++);
-   0 == resultsStore && showError("There is nothing here"), chooseBar(resultsStore)
+   0 == resultsStore && showError("There is nothing here"), selectLoc(resultsStore)
 }
 
-function chooseBar(a) {
-   barRef = {
+function selectLoc(a) {
+   placeRef = {
       reference: a[dataCount].reference
-   }, service.getDetails(barRef, showBar)
+   }, service.getDetails(placeRef, showPlace)
 }
 
-function showBar(a, b) {
+function showPlace(a, b) {
    if (b == google.maps.places.PlacesServiceStatus.OK) {
       for (i in markersArray) markersArray[i].setMap(null);
       destMarker = new google.maps.Marker({
@@ -60,7 +60,7 @@ function showBar(a, b) {
          position: a.geometry.location,
          icon: destIcon
       }), markersArray.push(destMarker), placeName = a.name, routemap(userLoc, a.geometry.location), directionsDisplay.setMap(null), directionsDisplay.suppressMarkers = !0, directionsDisplay.setMap(map), placeSite = a.website ? a.website : a.url, placeAddress = a.formatted_address, $(".uDestination").html("You can try <br/><a href='" + placeSite + "' target='_blank'>" + placeName + '</a><a href ="' + a.url + '" class="rating star' + Math.ceil(a.rating) + '"><span class="">based on ' + a.user_ratings_total + ' ratings</span></a><br/>Call Them at <a href="tel:' + a.international_phone_number + '"> ' + a.international_phone_number + " </a>"), $(".mapAddress").html(placeAddress), $(".btmRow,.recommendation,.mapAddress,.uDestination").fadeIn(function() {
-         $wait.fadeOut()
+         fnWaiting.fadeOut()
       })
    }
 }
@@ -77,8 +77,8 @@ function routemap(a, b) {
 }
 
 function codeAddress() {
-   $wait.fadeIn(function() {
-      $locationBar.fadeOut();
+   fnWaiting.fadeIn(function() {
+      placeLoc.fadeOut();
       var a = document.getElementById("gLocation").value;
       geocoder.geocode({
          address: a
@@ -89,10 +89,10 @@ function codeAddress() {
 }
 
 function showError(a) {
-   $wait.fadeOut(), $locationBar.fadeIn(), $(".locationInfo").text(a).fadeIn()
+   fnWaiting.fadeOut(), placeLoc.fadeIn(), $(".locationInfo").text(a).fadeIn()
 }
 
-function removeDupes(a, b) {
+function Duplicates(a, b) {
    var c = [],
       d = {};
    for (var e in a) d[a[e][b]] = a[e];
@@ -105,8 +105,8 @@ var resultref, marker, markersArray = [],
    destIcon = "https://cloud.githubusercontent.com/assets/8266671/5778965/0f1e8bee-9dc5-11e4-8b68-2b27d80a2fd2.png",
    homeIcon = "https://cloud.githubusercontent.com/assets/8266671/5778967/0fb3649e-9dc5-11e4-90a9-cd7f2f2456fe.png",
    resultsStore, totalResults = [],
-   pubResultsStore, barResultsStore, userLoc, currentlatlng;
-$wait = $(".loading"), $locationBar = $(".locator"), $mapCanvas = $(".mapCanvas");
+   resultsArray, resultsArrayCol, userLoc, currentlatlng;
+fnWaiting = $(".loading"), placeLoc = $(".locator"), $mapCanvas = $(".mapCanvas");
 var lowSat = [{
       featureType: "all",
       stylers: [{
@@ -131,7 +131,7 @@ var service = new google.maps.places.PlacesService(map),
 trafficLayer.setMap(map), $(document).ready(function() {
    var a = window.location.href,
       b = a.split("?");
-   "location" == b[1] ? $locationBar.css("opacity", 1) : getLocation()
+   "location" == b[1] ? placeLoc.css("opacity", 1) : getLocation()
 });
 var directionsDisplay = new google.maps.DirectionsRenderer({
       suppressMarkers: !0
@@ -139,8 +139,8 @@ var directionsDisplay = new google.maps.DirectionsRenderer({
    directionsService = new google.maps.DirectionsService;
 $(".locator").on("submit", function(a) {
    utilityOption(), codeAddress(), a.preventDefault()
-}), $(".repeatCount").click(function() {
-   return dataCount < resultsStore.length - 1 ? dataCount++ : dataCount = 0, chooseBar(resultsStore), window.scroll(0, 0), !1
+}), $(".resultRe").click(function() {
+   return dataCount < resultsStore.length - 1 ? dataCount++ : dataCount = 0, selectLoc(resultsStore), window.scroll(0, 0), !1
 });
 var autoOptions = {
       types: ["geocode"]
